9709ee21ba259f661d5b8c572040b2df5c60bbf1
Hasireddy <malikireddy.uma@gmail.com>
2026-02-26 15:38:28 +0100

    Book class created

--- data_models.py
+++ data_models.py
@@ -4,9 +4,29 @@ db = SQLAlchemy()
 
 
 class Author(db.Model):
+    __tablename__ = 'authors'
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100))
     birth_date = db.Column(db.Date)
 
     def __repr__(self):
         return f"Author: {self.name}"
+
+
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(100))
+    title = db.Column(db.String(100))
+    publication_year = db.Column(db.Integer)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+
+    def __repr__(self):
+        return f"Book: {self.title}"
+
+
+
